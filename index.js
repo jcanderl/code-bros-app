bd9c381d982b59b6858e5bd07c691f8faa083024
JC <jc@profitpeakmarketing.com>
2015-12-29 21:52:23 -0600

    add post to /signups

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 var express = require('express');
 var app = express();
 var pg = require('pg');
+var bodyParser = require('body-parser');
+
+app.use(bodyParser.json({ type: 'application/json' }));
 
 app.set('port', (process.env.PORT || 5000));
 
@@ -28,6 +31,31 @@ var sql = 'SELECT * FROM signups';
     });
   });
 });
+
+app.post('/signups', function(req, res) {
+  pg.connect(process.env.DATABASE_URL, function(err, client, done) {
+    var sql = "INSERT INTO signups (id, name, email) values ($1, $2, $3) RETURNING id"; 
+    var data = [
+      req.body.id,
+      req.body.name,
+      req.body.email
+    ];
+    client.query(sql, data, function(err, result) {
+      done();
+      if (err) {
+       console.error(err);
+        res.statusCode = 500;
+        return res.json({
+          errors: ['Could not create signup']
+      }); }
+      else {
+        res.statusCode = 201;
+        res.json(result.rows[0]);
+      }
+    });
+  });
+});
+
 
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
